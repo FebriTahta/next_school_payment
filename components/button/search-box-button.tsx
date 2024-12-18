bc0406c4bb552d2aa8bd920f31e60ee359d4ce25
FebriTahta <febrirtah@gmail.com>
2024-12-18 12:51:27 +0700

    penyesuaan posisi menu bottom

--- components/button/search-box-button.tsx
+++ components/button/search-box-button.tsx
@@ -17,7 +17,7 @@ const SearchBoxButton = () => {
   };
 
   return (
-    <button type="button" className="relative w-full max-w-full">
+    <div className="w-full max-w-full">
       <div className="flex items-center space-x-2 px-4 py-2 bg-gray-100 dark:bg-gray-800 rounded-lg shadow-sm">
         <Search className="w-5 h-5 text-gray-500 dark:text-gray-400" />
         <input
@@ -47,7 +47,7 @@ const SearchBoxButton = () => {
           </ul>
         </div>
       )}
-    </button>
+    </div>
   );
 };
 
