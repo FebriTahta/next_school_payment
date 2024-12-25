03ba9a6d95dd841330fbd72a1a466adf1053f291
FebriTahta <febrirtah@gmail.com>
2024-12-25 18:11:30 +0700

    custom search params

--- app/payment-list/page.tsx
+++ app/payment-list/page.tsx
@@ -27,12 +27,20 @@ const fetchPaymentDetails = async (searchParams: SearchParams) => {
   };
 };
 
-export default async function Page({
-  searchParams,
-}: {
-  searchParams: { [key: string]: string | string[] | undefined }
-}) {
-  const typedSearchParams: SearchParams = searchParams as SearchParams;
+export interface PageProps {
+  params: { slug: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+}
+
+export default async function Page({ searchParams }: PageProps) {
+  const typedSearchParams: SearchParams = {
+    payment_type: searchParams.payment_type as string | undefined,
+    i_pay: searchParams.i_pay as string | undefined,
+    nis: searchParams.nis as string | undefined,
+    kd_rombel: searchParams.kd_rombel as string | undefined,
+    token: searchParams.token as string | undefined,
+  };
+
   const props = await fetchPaymentDetails(typedSearchParams);
 
   return (
