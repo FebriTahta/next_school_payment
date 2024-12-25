02583124cf831e45751c816cabd7af9a2e4b22c4
FebriTahta <febrirtah@gmail.com>
2024-12-25 17:59:55 +0700

    fix app router

--- app/payment-list/page.tsx
+++ app/payment-list/page.tsx
@@ -1,6 +1,14 @@
 import PaymentList from "@/components/page/payment-list";
 
-const fetchPaymentDetails = async (searchParams: { [key: string]: string | undefined }) => {
+interface SearchParams {
+  payment_type?: string;
+  i_pay?: string;
+  nis?: string;
+  kd_rombel?: string;
+  token?: string;
+}
+
+const fetchPaymentDetails = async (searchParams: SearchParams) => {
   const payment_type = atob(searchParams.payment_type || '-');
   const i_pay = searchParams.i_pay || 'List';
   const nis = atob(searchParams.nis || '-');
@@ -10,15 +18,20 @@ const fetchPaymentDetails = async (searchParams: { [key: string]: string | undef
   return {
     title: `Active Payment : ${payment_type.toUpperCase()}`,
     desc: "Make payment before the due date passes",
-    nis: nis,
-    kd_rombel: kd_rombel,
-    payment_type: payment_type,
-    i_pay: i_pay,
-    token: token,
+    nis,
+    kd_rombel,
+    payment_type,
+    i_pay,
+    token,
   };
 };
 
-const Page = async ({ searchParams }: { searchParams: Record<string, string | undefined> }) => {
+const Page = async ({
+  searchParams,
+}: {
+  searchParams: SearchParams;
+}) => {
+  // Parsing searchParams
   const props = await fetchPaymentDetails(searchParams);
 
   return (
