a50dbb0ef241f4793782c7b2866f336e34f564bd
FebriTahta <febrirtah@gmail.com>
2024-12-25 17:55:53 +0700

    tidak perlu membuat instance URLSearchParams secara manual.

--- app/payment-list/page.tsx
+++ app/payment-list/page.tsx
@@ -1,11 +1,11 @@
 import PaymentList from "@/components/page/payment-list";
 
-const fetchPaymentDetails = async (searchParams: URLSearchParams) => {
-  const payment_type = atob(searchParams.get('payment_type') || '-');
-  const i_pay = searchParams.get('i_pay') || 'List';
-  const nis = atob(searchParams.get('nis') || '-');
-  const kd_rombel = atob(searchParams.get('kd_rombel') || '-');
-  const token = atob(searchParams.get('token') || '-');
+const fetchPaymentDetails = async (searchParams: { [key: string]: string | undefined }) => {
+  const payment_type = atob(searchParams.payment_type || '-');
+  const i_pay = searchParams.i_pay || 'List';
+  const nis = atob(searchParams.nis || '-');
+  const kd_rombel = atob(searchParams.kd_rombel || '-');
+  const token = atob(searchParams.token || '-');
 
   return {
     title: `Active Payment : ${payment_type.toUpperCase()}`,
@@ -18,7 +18,7 @@ const fetchPaymentDetails = async (searchParams: URLSearchParams) => {
   };
 };
 
-const Page = async ({ searchParams }: { searchParams: URLSearchParams }) => {
+const Page = async ({ searchParams }: { searchParams: { [key: string]: string | undefined } }) => {
   const props = await fetchPaymentDetails(searchParams);
 
   return (
